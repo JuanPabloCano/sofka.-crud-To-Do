930b1165a423d04fb036176d1f9ba78d145615a5
Juan Pablo Cano <Canojuan0415@gmail.com>
2021-11-24 19:58:24 -0500

    Se desarrollan los métodos para actualizar y eliminar registros

--- frontend-crud-react/src/App.js
+++ frontend-crud-react/src/App.js
@@ -1,17 +1,20 @@
 import React, { createContext, useContext, useReducer, useEffect, useRef, useState } from 'react';
+import { container, row, col } from 'reactstrap';
+import { table, button} from 'reactstrap';
 
 const HOST_API = "http://localhost:8080/api";
 
 const initialState = {
-  list: []
-}
+  list: [],
+  item: {}
+};
 
 const Store = createContext(initialState)
 
 const Form = () => {
   const formRef = useRef(null);
-  const { dispatch } = useContext(Store);
-  const [state, setState] = useState({});
+  const { dispatch, state: { item } } = useContext(Store);
+  const [state, setState] = useState(item);
 
   const onAdd = (event) => {
     event.preventDefault();
@@ -36,11 +39,39 @@ const Form = () => {
         formRef.current.reset();
       });
   }
+
+  const onEdit = (event) => {
+    event.preventDefault();
+
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted
+    };
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  }
+
   return <form ref={formRef}>
-    <input type="text" name="name" onChange={(event) => {
+    <input type="text" name="name" defaultValue={item.name} onChange={(event) => {
       setState({ ...state, name: event.target.value })
     }}></input>
-    <button onClick={onAdd}>Agregar</button>
+
+    {item.id && <button onClick={onEdit}>Actualizar</button>}
+    {!item.id && <button onClick={onAdd}>Agregar</button>}
+
   </form>
 }
 const List = () => {
@@ -54,11 +85,23 @@ const List = () => {
       })
   }, [state.list.length, dispatch]);
 
-  return <div>
-    <table>
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE"
+    })
+      .then((list) => {
+        dispatch({ type: "delete-item", id })
+      })
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo })
+  };
+  return <div className = "container mt-10">
+    <table className = "table table-striped table-dark">
       <thead>
         <tr>
-          <td>ID</td>
+          <td className = "table-primary">ID</td>
           <td>Nombre</td>
           <td>¡¿Está completado?</td>
         </tr>
@@ -68,7 +111,9 @@ const List = () => {
           return <tr key={todo.id}>
             <td>{todo.id}</td>
             <td>{todo.name}</td>
-            <td>{todo.isCompleted}</td>
+            <td>{todo.isCompleted === true ? "SI" : "NO"}</td>
+            <td><button onClick={() => onDelete(todo.id)} className ="btn btn-primary">Eliminar</button></td>
+            <td><button onClick={() => onEdit(todo)} className ="btn btn-primary">Editar</button></td>
           </tr>
         })}
       </tbody>
@@ -78,12 +123,32 @@ const List = () => {
 
 function reducer(state, action) {
   switch (action.type) {
+
+    case 'update-item':
+      const listUpdateEdit = state.list.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item;
+        }
+        return item;
+      });
+      return { ...state, list: listUpdateEdit, item: {} }
+
+    case 'delete-item':
+      const listUpdate = state.list.filter((item) => {
+        return item.id !== action.id;
+      });
+
     case 'update-list':
       return { ...state, list: action.list }
+
+    case 'edit-item':
+      return { ...state, item: action.item }
+
     case 'add-item':
       const newList = state.list;
       newList.push(action.item);
       return { ...state, list: newList }
+
     default:
       return state;
   }
@@ -99,8 +164,8 @@ const StoreProvider = ({ children }) => {
 
 function App() {
   return <StoreProvider>
-    <Form/>
-    <List/>
+    <Form />
+    <List />
   </StoreProvider>
 }
 
